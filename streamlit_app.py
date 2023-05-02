a1ea7bd5f1273686b3472ab0b8e8484bfcb67457
TAECHANGEOM <132345505+TAECHANGEOM@users.noreply.github.com>
2023-05-02 18:36:27 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega3 & Blueberry oatmeal')
+streamlit.text('kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
